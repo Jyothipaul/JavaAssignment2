fb59bb73fe254419a1de5c3070a1ce5153d23c03
jyothipaul@yahoo.com <jyothipaul@yahoo.com>
2019-01-05 23:41:58 +0000

    Java Assignment 2

---
+++ src/MainOperations.java
@@ -0,0 +1,15 @@
+import loveProgramming.Operations;
+
+public class MainOperations {
+
+   public static void main(String[] args) {
+
+       Operations Op1 = new Operations();
+
+       int k = Op1.add(125,75);
+       System.out.println("Sum of two number is: " + k);
+
+       int z = Op1.sub(85,75);
+       System.out.println("Difference of two number is: " + z);
+    }
+}
